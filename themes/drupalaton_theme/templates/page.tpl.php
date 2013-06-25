4249a2615bebaff14f7500387a87e95349d34e5a
Csáki István <istvan@drupart.hu>
2013-06-25 20:28:40 +0200

    add container 12 to page contents

--- themes/drupalaton_theme/templates/page.tpl.php
+++ themes/drupalaton_theme/templates/page.tpl.php
@@ -29,33 +29,32 @@
         <a itemprop="name" title="Drupalaton" href="/"></a>
       </div>
     </div>
-  </div>
+  </div><!-- /.landscape-->
+  
+  <div class="container_12">
 
-  <?php if ($breadcrumb): ?>
-    <div id="breadcrumb"><?php print $breadcrumb; ?></div>
-  <?php endif; ?>
-
-  <?php print $messages; ?>
-
-  <div id="main-wrapper"><div id="main" class="clearfix">
-
-    <div id="content" class="column"><div class="section">
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
-    </div></div> <!-- /.section, /#content -->
-
-  </div></div> <!-- /#main, /#main-wrapper -->
-
-  <div id="footer"><div class="section">
-    <?php print render($page['footer']); ?>
-  </div></div> <!-- /.section, /#footer -->
+    <?php if ($breadcrumb): ?>
+      <div id="breadcrumb"><?php print $breadcrumb; ?></div>
+    <?php endif; ?>
+  
+    <?php print $messages; ?>
+  
+    <div id="main-wrapper"><div id="main" class="clearfix">
+  
+      <div id="content" class="column"><div class="section">
+        <a id="main-content"></a>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+        <?php print render($page['content']); ?>
+        <?php print $feed_icons; ?>
+      </div></div> <!-- /.section, /#content -->
+  
+    </div></div> <!-- /#main, /#main-wrapper -->
+  </div> <!-- /.container_12 -->
 
 </div>
 <div class="newsletter" id="newsletter">
